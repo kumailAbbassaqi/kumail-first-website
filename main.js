8a8bb116ee0ee53582a11c05ca60828e613e8430
kumailAbbassaqi <168869497+kumailAbbassaqi@users.noreply.github.com>
2024-09-07 20:56:35 +0500

    Add files via upload
    🌐 My First Web Development Project
    Welcome to my very first web development adventure! 🚀
    This project is all about exploring the basics of HTML, CSS, and JavaScript. I’ve built a simple yet engaging webpage to sharpen my skills and bring my ideas to life. 🎨
    
    From designing clean layouts to adding a touch of interactivity, this project reflects the beginning of my journey into the world of web development. 🌟 I’m excited to learn and grow, and this is just the first of many creations!
    
    Feel free to explore and give feedback! 😊

---
+++ main.js
@@ -0,0 +1,62 @@
+//  change navbar style on scroll
+
+
+window.addEventListener('scroll' , () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll',window.scrollY > 0)
+})
+ 
+
+//hide the faqs answer and also to show
+
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open')    
+
+        //icon change
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus";
+        } else {
+            icon.className = 'uil uil-plus';
+        }
+    } )
+})
+
+//nav hide and show 
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click' , () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+
+})
+
+//close nav menu 
+const closeNav = () => {   
+     menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click' , closeNav);
+
+// theme changer 
+
+var icon = document.querySelector("#icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("theme");
+    if(document.body.classList.contains("theme")) {
+        icon.src = "images/tm1.jpg";
+    }
+        else{
+            icon.src = "images/tm2.jpg";
+        }
+   
+}
+
